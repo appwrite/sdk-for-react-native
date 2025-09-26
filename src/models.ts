762df455763874846ab7cd1fd63ab919c755eba5
root <chiragaggarwal5k@gmail.com>
2025-09-26 09:06:24 +0000

    chore: regenerate sdks

--- src/models.ts
+++ src/models.ts
@@ -1,3 +1,6 @@
+import { ExecutionTrigger } from "./enums/execution-trigger"
+import { ExecutionStatus } from "./enums/execution-status"
+
 export namespace Models {
 
     declare const __default: unique symbol;
@@ -1008,11 +1011,11 @@ export namespace Models {
         /**
          * The trigger that caused the function to execute. Possible values can be: `http`, `schedule`, or `event`.
          */
-        trigger: string;
+        trigger: ExecutionTrigger;
         /**
          * The status of the function execution. Possible values can be: `waiting`, `processing`, `completed`, or `failed`.
          */
-        status: string;
+        status: ExecutionStatus;
         /**
          * HTTP request method type.
          */
