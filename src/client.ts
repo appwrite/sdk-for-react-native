767fae97ef1f0c187f8819a4971a3f5c8b46396c
root <arnabchatterjee.ac.2@gmail.com>
2026-03-03 10:09:33 +0000

    regen

--- src/client.ts
+++ src/client.ts
@@ -158,7 +158,7 @@ class Client {
         'x-sdk-name': 'React Native',
         'x-sdk-platform': 'client',
         'x-sdk-language': 'reactnative',
-        'x-sdk-version': '0.24.2',
+        'x-sdk-version': '1.0.0',
         'X-Appwrite-Response-Format': '1.8.0',
     };
 
