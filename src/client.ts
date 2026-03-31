bb52078b72a7fbc9a98bc34bc865f73b600e9de8
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-03-31 17:10:42 +0530

    chore: update React Native SDK to 0.27.0

--- src/client.ts
+++ src/client.ts
@@ -161,9 +161,13 @@ class Client {
         'x-sdk-name': 'React Native',
         'x-sdk-platform': 'client',
         'x-sdk-language': 'reactnative',
-        'x-sdk-version': '0.26.0',
+        'x-sdk-version': '0.27.0',
         'X-Appwrite-Response-Format': '1.9.0',
     };
+
+    getHeaders(): Headers {
+        return { ...this.headers };
+    }
 
     /**
      * Set Endpoint
